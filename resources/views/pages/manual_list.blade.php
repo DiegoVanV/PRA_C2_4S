8b1ed29b429f8db91a610bc58b9cfd1e26e632e3
DiegoVanV <159238577+DiegoVanV@users.noreply.github.com>
2024-09-26 11:31:04 +0200

    ticket5done

--- resources/views/pages/manual_list.blade.php
+++ resources/views/pages/manual_list.blade.php
@@ -8,22 +8,35 @@
         <li><a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/" alt="Manuals for '{{$brand->name}}'" title="Manuals for '{{$brand->name}}'">{{ $brand->name }}</a></li>
     </x-slot:breadcrumb>
 
-
     <h1>{{ $brand->name }}</h1>
 
     <p>{{ __('introduction_texts.type_list', ['brand'=>$brand->name]) }}</p>
 
+    <?php
+    $manualCount = count($manuals);
+    $columns = 3;
+    $chunk_size = ceil($manualCount / $columns);
+    ?>
 
-        @foreach ($manuals as $manual)
-
-            @if ($manual->locally_available)
-                <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
-                ({{$manual->filesize_human_readable}})
-            @else
-                <a href="{{ $manual->url }}" target="new" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
-            @endif
-
-            <br />
-        @endforeach
+    <div class="container">
+        <div class="row">
+            @foreach ($manuals->chunk($chunk_size) as $chunk)
+                <div class="col-md-4">
+                    <ul>
+                        @foreach ($chunk as $manual)
+                            <li>
+                                @if ($manual->locally_available)
+                                    <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
+                                    ({{$manual->filesize_human_readable}})
+                                @else
+                                    <a href="{{ $manual->url }}" target="_blank" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
+                                @endif
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endforeach
+        </div>
+    </div>
 
 </x-layouts.app>
